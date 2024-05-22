72b5425f786fbf7f0c3990ba413f36f608cd86ab
IlyinIO <ilyin88ok@gmail.com>
2024-05-22 14:57:56 +0700

    1.First step 2.Add two tasks 3. Make easy coding 4. Using LinkedHashSet 5. Delete for PullRequest

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,18 @@
 import java.util.*;
 
-import static java.util.Collections.sort;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        for (Integer numOne : nums) {
-            if (numOne % 2 != 0) {
-                System.out.print(numOne);
-            }
-        }
+
         System.out.println("");
         System.out.println("Task 2");
-        Set<Integer> numb = new HashSet<>(nums);
-        for (Integer numTwo : numb) {
-            if (numTwo % 2 != 1) {
-                System.out.print(numTwo);
-            }
-        }
+
         System.out.println("");
         System.out.println("Task 3");
-        List<String> words = new ArrayList<>(List.of("один", "один", "два", "три",
-               "четыре", "четыре", "пять", "пять", "шесть", "семь"));
-        Set<String> wordOne = new LinkedHashSet<>(words);
-        System.out.println(wordOne);
 
         System.out.println("");
         System.out.println("Task 4");
-        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
-        Set<String> wordTwo = new LinkedHashSet<>(strings);
-        for (String k : wordTwo) {
-            int count = Collections.frequency(strings, k);
-            System.out.println(k + ": " + count);
-        }
+
 
     }
 
