4464158d3053c02f244a64095a8f3231cfc34d0a
IlyinIO <ilyin88ok@gmail.com>
2024-05-22 13:11:35 +0700

    1.First step 2.Add two tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
+import static java.util.Collections.sort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Task 1");
+        List<Integer> numsOne = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Iterator<Integer> numsItOne = numsOne.iterator();
+        while (numsItOne.hasNext()) {
+            Integer num = numsItOne.next();
+            if(num % 2 == 0) {
+                numsItOne.remove();
+            }
+        }
+        System.out.println(numsOne);
+
+        System.out.println("Task 2");
+        List<Integer> numsTwo = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Iterator<Integer> numsItTwo = numsTwo.iterator();
+        while (numsItTwo.hasNext()) {
+            Integer num = numsItTwo.next();
+            if(num % 2 != 0) {
+                numsItTwo.remove();
+            }
+        }
+        sort(numsTwo);
+        System.out.println(numsTwo);
+
+        System.out.println("Task 3");
+
     }
 }
